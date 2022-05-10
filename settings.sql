12163f279a00d69799bb951bc1e2992ff28007d0
timmellis <tim.m.ellis@gmail.com>
2022-05-10 17:58:56 -0400

    tweaks to fix bugs in auth

--- settings.sql
+++ settings.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE greenthumb_with_auth;
 CREATE USER tim WITH PASSWORD 'greenthumbadminpw1129';
-GRANT ALL PRIVILEGES ON DATABASE greenthumb TO tim;
+GRANT ALL PRIVILEGES ON DATABASE greenthumb_with_auth TO tim;
