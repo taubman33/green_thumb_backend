8e695350a73699a53e4bbd4a5b7083cb2a37dbb6
timmellis <tim.m.ellis@gmail.com>
2022-05-15 21:25:20 -0400

    hide secret keys

--- greenthumb_django/settings.py
+++ greenthumb_django/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t_9s3qge@9g-6vsuj0#w^5%(osfy_)3tqjle_+acdb)f8#+a4c'
+SECRET_KEY = 'removed'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,13 +139,6 @@ WSGI_APPLICATION = 'greenthumb_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'greenthumb_with_auth',
-    #     'USER': 'admin',
-    #     'PASSWORD': 'greenthumbadminpw1129',
-    #     'HOST': 'localhost'
-    # }
 }
 
 try:
